829d6930cb9713613afaca3f04cb38135cc99bf1
Martijn Bogaert <martijn.bogaert@student.hogent.be>
2021-08-19 14:58:45 +0200

    Add initial project files

---
+++ src/main/java/com/martijnbogaert/summercamp/SummerCampApplication.java
@@ -0,0 +1,13 @@
+package com.martijnbogaert.summercamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerCampApplication.class, args);
+	}
+
+}
